cdd3b8a12eeee3204440a2bd4970fb07bf648465
7Spike35 <151526293+7Spike35@users.noreply.github.com>
2024-07-12 12:01:12 +0800

    Delete code/django_web directory

--- code/django_web/login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import login
-
-urlpatterns=[
-    path('login/', login,name='login'),
-]
--- code/django_web/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import register
-
-
-
-urlpatterns=[
-    path('register/',register)
-]
